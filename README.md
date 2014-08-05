5b306a3731f6713a3ad8f45db18f6aa9825783e8
R. S. Doiel <rsdoiel@gmail.com>
2014-08-05 15:43:11 -0700

    Initial implementation

--- README.md
+++ README.md
@@ -1,4 +1,60 @@
 reldate
 =======
 
-A small utility which returns the relative date in YYYY-MM-DD format.
+A small command line utility which returns the relative date in YYYY-MM-DD format. This is helpful
+when scripting various time relationships.
+
+## Example
+
+If today was 2014-08-03 and you wanted the date three days in the past try--
+
+```
+    reldate 3 days
+```
+
+The output would be 
+
+```
+    2014-08-06
+```
+
+## Time units
+
+Supported time units are
+
++ day(s)
++ week(s)
++ year(s)
+
+## Specifying a date to calucate from
+
+_reldate_ handles dates in the YYYY-MM-DD format (e.g. March 1, 2014 would be 2014-03-01).  By default _reldate_ uses today as
+the date to calculate relative time from.  If you use the *--from* option you can it will calculate the relative date from that 
+specific date. 
+
+```
+    reldate --from=2014-08-03 3 days
+```
+
+Will yield
+
+```
+    2014-08-06
+```
+
+## Negative increments
+
+Command line arguments traditionally start with a dash which we also use to denote a nagative number. To tell the command line
+process that to not treat negative numbers as an "option" preceed your time increment and time unit with a double dash.
+
+```
+   reldate --from=2014-08-03 -- -3 days 
+```
+
+Will yield
+
+```
+    2014-07-31
+```
+
+
---
+++ reldate.go
@@ -0,0 +1,119 @@
+/**
+ * Generates a date in YYYY-MM-DD format based on a relative time
+ * description (e.g. -1 week, +3 years)
+ */
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+    "strconv"
+    "strings"
+    "time"
+)
+
+var (
+    help bool
+    relative_to string
+    relative_t time.Time 
+    time_inc int
+    time_unit int
+)
+
+var Usage = func(exit_code int, msg string) {
+	fmt.Fprintf(os.Stderr, `%s
+ USAGE %s TIME_INCREMENT TIME_UNIT
+
+ EXAMPLES
+ 
+ Two days from today: %s 2 days
+ Three weeks ago: %s -- -3 weeks
+ Three weeks from 2014-01-01: %s --from=2014-01-01 3 weeks
+ Three days before 2014-01-01: %s --from=2014-01-01 -- -3 days
+
+ Time increments are a positive or negative integer. Time unit can be
+ either day(s), week(s), month(s), or year(s).
+
+ OPTIONS
+
+`, msg, os.Args[0], os.Args[0], os.Args[0], os.Args[0], os.Args[0])
+
+	flag.PrintDefaults()
+
+	fmt.Fprintf(os.Stderr, `
+
+ copyright (c) 2014
+ Released under the Simplified BSD License
+ See: http://opensource.org/licenses/bsd-license.php
+
+`)
+	os.Exit(exit_code)
+}
+
+func init() {
+	const (
+        relative_to_usage = "Date the relative time is calculated from."
+		help_usage = "Display this help document."
+	)
+
+    flag.StringVar(&relative_to, "from", relative_to, relative_to_usage)
+    flag.StringVar(&relative_to, "f", relative_to, relative_to_usage)
+	flag.BoolVar(&help, "help", help, help_usage)
+	flag.BoolVar(&help, "h", help, help_usage)
+}
+
+func assertOk(e error, fail_msg string) {
+    if e != nil {
+        Usage(1, fmt.Sprintf(" %s\n %s\n", fail_msg, e))
+    }
+}
+
+func relativeTime(t time.Time, i int, u string) (time.Time, error) {
+    switch {
+        case strings.HasPrefix(u, "day"):
+            return t.AddDate(0, 0, i,), nil;
+        case strings.HasPrefix(u, "week"):
+            return t.AddDate(0, 0, 7 * i), nil;
+        case strings.HasPrefix(u, "month"):
+            return t.AddDate(0, i, 0), nil;
+        case strings.HasPrefix(u, "year"):
+            return t.AddDate(i, 0, 0), nil;
+
+    }
+    return t, errors.New("Time unit must be day(s), week(s), month(s) or year(s).")
+}
+
+func main() {
+    const yyyymmdd = "2006-01-02"
+    var err error
+
+    //FIXME: How do I ignore -# so negative values are easy to process
+    // as time increments.
+	flag.Parse()
+	if help == true {
+		Usage(0, "")
+	}
+    
+    argc := flag.NArg()
+    argv := flag.Args()
+
+    if argc < 2 {
+        Usage(1, "Missing time increment or units.")
+    } else if argc > 2 {
+        Usage(1, "Too many command line arguments.")
+    }
+
+    relative_t = time.Now()
+    if relative_to != "" {
+        relative_t, err = time.Parse(yyyymmdd, relative_to)
+        assertOk(err, "Cannot parse the from date.")
+    }
+    time_inc, err := strconv.Atoi(argv[0])
+    assertOk(err, "Time increment should be a positive or negative integer")
+    t, err := relativeTime(relative_t, time_inc, argv[1])
+    assertOk(err, "Time unit should be either day(s), week(s), month(s) or year(s)")
+
+	fmt.Println(t.Format(yyyymmdd))
+}
